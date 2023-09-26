ba08f793a85c582a6fb01f790d8d2bc4ea6e05e2
langerp <patrick.langer@hotmail.de>
2023-09-26 10:14:45 +0200

    Add left shift to gaming layer

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -16,7 +16,7 @@
 LAYOUT_split_3x6_3( \
 TO(U_GAME),  K00,  K01,  K02,  K03,  K04,         K05,  K06,  K07,  K08,  K09,  XXX, \
 TO(U_BASE),  K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19,  XXX, \
-XXX,  K20,  K21,  K22,  K23,  K24,                K25,  K26,  K27,  K28,  K29,  XXX , \
+KC_LSFT,  K20,  K21,  K22,  K23,  K24,                K25,  K26,  K27,  K28,  K29,  XXX , \
                   K32,  K33,  K34,                K35,  K36,  K37 \
 )
 
