b34d0f2578552517058df7775bb628bd88a9462a
langerp <patrick.langer@hotmail.de>
2025-12-03 18:02:36 +0100

    Modify keymap configuration for Miryoku layout

--- keyboards/crkbd/rev4_0/standard/keymaps/manna-harbour_miryoku/config.h
+++ keyboards/crkbd/rev4_0/standard/keymaps/manna-harbour_miryoku/config.h
@@ -15,7 +15,7 @@
 ) \
 LAYOUT_split_3x6_3_ex2( \
 XXX,  K00,  K01,  K02,  K03,  K04,  XXX,         XXX,  K05,  K06,  K07,  K08,  K09,  XXX, \
-XXX,  K10,  K11,  K12,  K13,  K14,  XXX,         XXX,  K15,  K16,  K17,  K18,  K19,  XXX, \
-XXX,  K20,  K21,  K22,  K23,  K24,                     K25,  K26,  K27,  K28,  K29,  XXX , \
+KC_LSFT,  K10,  K11,  K12,  K13,  K14,  XXX,         XXX,  K15,  K16,  K17,  K18,  K19,  XXX, \
+KC_LCTL,  K20,  K21,  K22,  K23,  K24,                     K25,  K26,  K27,  K28,  K29,  XXX , \
                   K32,  K33,  K34,                     K35,  K36,  K37 \
 )
